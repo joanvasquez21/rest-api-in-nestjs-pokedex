12cd0d5484dbf4c170fb23bea6ec20f2c552a522
joanvasquez21 <joanvrivas.21@gmail.com>
2024-08-31 14:37:07 +0200

    the connection to mongodb is added

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,8 +9,7 @@ import { MongooseModule } from '@nestjs/mongoose';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-
-    MongooseModule.forRoot('mongodb:localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest-pokemon'),
 
     PokemonModule
   ],
